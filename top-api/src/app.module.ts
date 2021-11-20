6721523dbcc7eb5aec0e5883832b64fc44c66af1
kafanasiev <kafanasiev@sistema-capital.com>
2021-11-20 10:10:39 +0300

    Add mongo connection

--- top-api/src/app.module.ts
+++ top-api/src/app.module.ts
@@ -1,14 +1,28 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config'
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { PageModule } from './page/page.module';
 import { ReviewModule } from './review/review.module';
 import { ProductModule } from './product/product.module';
+import { TypegooseModule } from 'nestjs-typegoose';
+import { getMongoConfig } from './configs/mongo.config';
 
 @Module({
-  imports: [AuthModule, PageModule, ReviewModule, ProductModule],
+  imports: [
+    ConfigModule.forRoot(),
+    TypegooseModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: getMongoConfig
+    }),
+    AuthModule,
+    PageModule,
+    ReviewModule,
+    ProductModule
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
