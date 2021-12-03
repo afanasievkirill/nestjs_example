f988b0251ad6312ea1888dc0fac5c354f5d5a75b
kafanasiev <kafanasiev@sistema-capital.com>
2021-12-03 11:40:42 +0300

    auth tests

---
+++ top-api/test/auth.e2e-spec.ts
@@ -0,0 +1,64 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { INestApplication } from '@nestjs/common';
+import * as request from 'supertest';
+import { AppModule } from './../src/app.module';
+import { disconnect } from 'mongoose';
+import { AuthDto } from 'src/auth/dto/auth.dto';
+import { USER_NOT_FOUND_ERROR, WRONG_PASSWORD_ERROR } from '../src/auth/auth.constans';
+
+const loginDto: AuthDto = {
+	login: 'test@test.ru',
+	password: '123456'
+};
+
+describe('AuthController (e2e)', () => {
+	let app: INestApplication;
+	let createdId: string;
+	let token: string;
+
+	beforeEach(async () => {
+		const moduleFixture: TestingModule = await Test.createTestingModule({
+			imports: [AppModule],
+		}).compile();
+
+		app = moduleFixture.createNestApplication();
+		await app.init();
+	});
+
+	it('/auth/login (POST) - success', async (done) => {
+		return request(app.getHttpServer())
+			.post('/auth/login')
+			.send(loginDto)
+			.expect(200)
+			.then(({ body }: request.Response) => {
+				expect(body.access_token).toBeDefined();
+				done();
+			});
+	});
+
+	it('/auth/login (POST) - fail password', () => {
+		return request(app.getHttpServer())
+			.post('/auth/login')
+			.send({ ...loginDto, password: '2' })
+			.expect(401, {
+				statusCode: 401,
+				message: WRONG_PASSWORD_ERROR,
+				error: "Unauthorized"
+			});
+	});
+
+	it('/auth/login (POST) - fail password', () => {
+		return request(app.getHttpServer())
+			.post('/auth/login')
+			.send({ ...loginDto, login: 'aaa@a.ru' })
+			.expect(401, {
+				statusCode: 401,
+				message: USER_NOT_FOUND_ERROR,
+				error: "Unauthorized"
+			});
+	});
+
+	afterAll(() => {
+		disconnect();
+	});
+});
